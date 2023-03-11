a666ee486b114a61119da63fc9d0712595f7e521
José Victor Zeller <josevictorzg@gmail.com>
2023-03-11 19:33:27 -0300

    fix: add 'id' field to 'available_keys' table

--- priv/repo/migrations/20230310010424_create_available_keys_table.exs
+++ priv/repo/migrations/20230310010424_create_available_keys_table.exs
@@ -3,6 +3,7 @@ defmodule ExShortener.Repo.Migrations.CreateAvailableKeysTable do
 
   def change do
     create table("available_keys", primary_key: false) do
+      add(:id, :uuid, primary_key: true)
       add(:key_id, references("keys", type: :uuid))
 
       timestamps()
